aa334f7b3dc4165370d59bac3a1e558df3b7bdac
Jonathan <clemons.jonathan@gmail.com>
2022-12-22 21:27:31 -0600

    add remaps. Clean up packer & fugitive config

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -1,10 +1,10 @@
 vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
 
-local ThePrimeagen_Fugitive = vim.api.nvim_create_augroup("ThePrimeagen_Fugitive", {})
+local JClemons_Fugitive = vim.api.nvim_create_augroup("JClemons_Fugitive", {})
 
 local autocmd = vim.api.nvim_create_autocmd
 autocmd("BufWinEnter", {
-        group = ThePrimeagen_Fugitive,
+        group = JClemons_Fugitive,
         pattern = "*",
         callback = function()
             if vim.bo.ft ~= "fugitive" then
--- lua/j-clemons/remap.lua
+++ lua/j-clemons/remap.lua
@@ -10,3 +10,9 @@ vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
 vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
+
+vim.keymap.set('n', '<C-d>', '<C-d>zz')
+vim.keymap.set('n', '<C-u>', '<C-u>zz')
+
+vim.keymap.set('n', '<leader>w', vim.cmd.w)
+vim.keymap.set('n', '<leader>q', vim.cmd.q)
